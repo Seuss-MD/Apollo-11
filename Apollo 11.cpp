e610513aca38b2fc10dff6ee5eb7d6aa59a30eb1
Sam L. Evans <156741307+Seuss-MD@users.noreply.github.com>
2025-01-15 10:36:30 -0700

    fix the styling

--- Apollo 11.cpp
+++ Apollo 11.cpp
@@ -1,18 +1,18 @@
 /*************************************************************
- * 1. Name:
- *      -Sam Evans, Savannah Harvey-
- * 2. Assignment Name:
- *      Practice 02: Physics simulator
- * 3. Assignment Description:
- *      Compute how the Apollo lander will move across the screen
- * 4. What was the hardest part? Be as specific as possible.
- *      -a paragraph or two about how the assignment went for you-
- * 5. How long did it take for you to complete the assignment?
- *      -total time in hours: reading the assignment, submitting, etc.
- **************************************************************/
+* 1. Name:
+*      -Sam Evans, Savannah Harvey-
+* 2. Assignment Name:
+*      Practice 02: Physics simulator
+* 3. Assignment Description:
+*      Compute how the Apollo lander will move across the screen
+* 4. What was the hardest part? Be as specific as possible.
+*      Understanding the physics for the assignment
+* 5. How long did it take for you to complete the assignment?
+*      About 4 hours
+**************************************************************/
 
 #include <iostream>  // for CIN and COUT
-#include <cmath>
+#include <cmath>     // for pi, sin(), and cos()
 using namespace std;
 
 #define WEIGHT   15103.000   // Weight in KG
@@ -20,18 +20,18 @@ using namespace std;
 #define THRUST   45000.000   // Thrust of main engine, in Newtons (kg m/s^2)
 
 /***************************************************
- * COMPUTE DISTANCE
- * Apply inertia to compute a new position using the distance equation.
- * The equation is:
- *     s = s + v t + 1/2 a t^2
- * INPUT
- *     s : original position, in meters
- *     v : velocity, in meters/second
- *     a : acceleration, in meters/second^2
- *     t : time, in seconds
- * OUTPUT
- *     s : new position, in meters
- **************************************************/
+* COMPUTE DISTANCE
+* Apply inertia to compute a new position using the distance equation.
+* The equation is:
+*     s = s + v t + 1/2 a t^2
+* INPUT
+*     s : original position, in meters
+*     v : velocity, in meters/second
+*     a : acceleration, in meters/second^2
+*     t : time, in seconds
+* OUTPUT
+*     s : new position, in meters
+**************************************************/
 double computeDistance(double s, double v, double a, double t)
 {
    // get updated position
@@ -41,18 +41,18 @@ double computeDistance(double s, double v, double a, double t)
 }
  
 
- /**************************************************
-  * COMPUTE ACCELERATION
-  * Find the acceleration given a thrust and mass.
-  * This will be done using Newton's second law of motion:
-  *     f = m * a
-  * INPUT
-  *     f : force, in Newtons (kg * m / s^2)
-  *     m : mass, in kilograms
-  * OUTPUT
-  *     a : acceleration, in meters/second^2
-  ***************************************************/
-double computeAccel(double f, double m)
+/**************************************************
+* COMPUTE ACCELERATION
+* Find the acceleration given a thrust and mass.
+* This will be done using Newton's second law of motion:
+*     f = m * a
+* INPUT
+*     f : force, in Newtons (kg * m / s^2)
+*     m : mass, in kilograms
+* OUTPUT
+*     a : acceleration, in meters/second^2
+***************************************************/
+double computeAcceleration(double f, double m)
 {
    double acceleration;
 
@@ -63,20 +63,20 @@ double computeAccel(double f, double m)
 }
 
 
-  /***********************************************
-   * COMPUTE VELOCITY
-   * Starting with a given velocity, find the new
-   * velocity once acceleration is applied. This is
-   * called the Kinematics equation. The
-   * equation is:
-   *     v = v + a t
-   * INPUT
-   *     v : velocity, in meters/second
-   *     a : acceleration, in meters/second^2
-   *     t : time, in seconds
-   * OUTPUT
-   *     v : new velocity, in meters/second
-   ***********************************************/
+/***********************************************
+* COMPUTE VELOCITY
+* Starting with a given velocity, find the new
+* velocity once acceleration is applied. This is
+* called the Kinematics equation. The
+* equation is:
+*     v = v + a t
+* INPUT
+*     v : velocity, in meters/second
+*     a : acceleration, in meters/second^2
+*     t : time, in seconds
+* OUTPUT
+*     v : new velocity, in meters/second
+***********************************************/
 double computeVelocity (double velocity, double acceleration, double time)
 {
 
@@ -88,24 +88,24 @@ double computeVelocity (double velocity, double acceleration, double time)
 
 
 /***********************************************
- * COMPUTE VERTICAL COMPONENT
- * Find the vertical component of a velocity or acceleration.
- * The equation is:
- *     cos(a) = y / total
- * This can be expressed graphically:
- *      x
- *    +-----
- *    |   /
- *  y |  / total
- *    |a/
- *    |/
- * INPUT
- *     a : angle, in radians
- *     total : total velocity or acceleration
- * OUTPUT
- *     y : the vertical component of the total
- ***********************************************/
-double computeVerticalY(double angle, double total)
+* COMPUTE VERTICAL COMPONENT
+* Find the vertical component of a velocity or acceleration.
+* The equation is:
+*     cos(a) = y / total
+* This can be expressed graphically:
+*      x
+*    +-----
+*    |   /
+*  y |  / total
+*    |a/
+*    |/
+* INPUT
+*     a : angle, in radians
+*     total : total velocity or acceleration
+* OUTPUT
+*     y : the vertical component of the total
+***********************************************/
+double computeVertical(double angle, double total)
 {
    double verticalY;
 
@@ -115,25 +115,25 @@ double computeVerticalY(double angle, double total)
    return verticalY;
 }
 
-    /***********************************************
-     * COMPUTE HORIZONTAL COMPONENT
-     * Find the horizontal component of a velocity or acceleration.
-     * The equation is:
-     *     sin(a) = x / total
-     * This can be expressed graphically:
-     *      x
-     *    +-----
-     *    |   /
-     *  y |  / total
-     *    |a/
-     *    |/
-     * INPUT
-     *     a : angle, in radians
-     *     total : total velocity or acceleration
-     * OUTPUT
-     *     x : the vertical component of the total
-     ***********************************************/
-double computeX(double a, double total)
+/***********************************************
+* COMPUTE HORIZONTAL COMPONENT
+* Find the horizontal component of a velocity or acceleration.
+* The equation is:
+*     sin(a) = x / total
+* This can be expressed graphically:
+*      x
+*    +-----
+*    |   /
+*  y |  / total
+*    |a/
+*    |/
+* INPUT
+*     a : angle, in radians
+*     total : total velocity or acceleration
+* OUTPUT
+*     x : the vertical component of the total
+***********************************************/
+double computeHorizontal(double a, double total)
 {
    double x;
 
@@ -143,64 +143,65 @@ double computeX(double a, double total)
    return x;
 }
 
-     /************************************************
-      * COMPUTE TOTAL COMPONENT
-      * Given the horizontal and vertical components of
-      * something (velocity or acceleration), determine
-      * the total component. To do this, use the Pythagorean Theorem:
-      *    x^2 + y^2 = t^2
-      * where:
-      *      x
-      *    +-----
-      *    |   /
-      *  y |  / total
-      *    | /
-      *    |/
-      * INPUT
-      *    x : horizontal component
-      *    y : vertical component
-      * OUTPUT
-      *    total : total component
-      ***********************************************/
+/************************************************
+* COMPUTE TOTAL COMPONENT
+* Given the horizontal and vertical components of
+* something (velocity or acceleration), determine
+* the total component. To do this, use the Pythagorean Theorem:
+*    x^2 + y^2 = t^2
+* where:
+*      x
+*    +-----
+*    |   /
+*  y |  / total
+*    | /
+*    |/
+* INPUT
+*    x : horizontal component
+*    y : vertical component
+* OUTPUT
+*    total : total component
+***********************************************/
 double computeTotal(double x, double y)
 {
    double total;
 
-   // total = x^2 + y^2
+   // total = square root(x^2 + y^2)
    total = sqrt((x * x) + (y * y));
 
    return total;
 }
 
 
-      /*************************************************
-       * RADIANS FROM DEGEES
-       * Convert degrees to radians:
-       *     radians / 2pi = degrees / 360
-       * INPUT
-       *     d : degrees from 0 to 360
-       * OUTPUT
-       *     r : radians from 0 to 2pi
-       **************************************************/
+/*************************************************
+* RADIANS FROM DEGEES
+* Convert degrees to radians:
+*     radians / 2pi = degrees / 360
+* INPUT
+*     d : degrees from 0 to 360
+* OUTPUT
+*     r : radians from 0 to 2pi
+**************************************************/
 double getRadian(double d)
 {
-   double r;
+   // calculate pi
    double pi = 2 * asin(1.0);
 
-   //convert to radian
+   //convert degrees to radians
+   double r;
    r = d / 360 * 2 * pi;
 
    return r;
 }
 
-       /**************************************************
-        * PROMPT
-        * A generic function to prompt the user for a double
-        * INPUT
-        *      message : the message to display to the user
-        * OUTPUT
-        *      response : the user's response
-        ***************************************************/
+/**************************************************
+* PROMPT
+* A generic function to prompt the user for a double
+* INPUT
+*      message : the message to display to the user
+* OUTPUT
+*      response : the user's response
+***************************************************/
 double prompt(string message)
 {
    //display message
@@ -234,33 +235,28 @@ int main()
    double ddy;                 // Total vertical acceleration
    double v;                   // Total velocity
 
+   // get accelerationThrust
+   accelerationThrust = computeAcceleration(THRUST, WEIGHT);
+
    // Go through the simulator five times
-   
    for (int i = 0; i < 5; i++)
    {
-      // Hint: Update the position _before_ updating the velocity
-
-      // acceleration
-      accelerationThrust = computeAccel(THRUST, WEIGHT);
+      // get radians
       aRadians = getRadian(aDegrees);
-      ddx = computeX(aRadians, accelerationThrust);
-      ddy = computeVerticalY(aRadians, accelerationThrust) + GRAVITY;
+      
+      // get the total acceleration of the horizontal and vertical
+      ddx = computeHorizontal(aRadians, accelerationThrust);
+      ddy = computeVertical(aRadians, accelerationThrust) + GRAVITY;
       
 
-      //get position
+      //get the new position
       y = computeDistance(y, dy, ddy, t);
       x = computeDistance(x, dx, ddx, t);
       
-      // velocity
+      // get the new velocity
       dx = computeVelocity(dx, ddx, t);
       dy = computeVelocity(dy, ddy, t);
-
-      // get the change in x and y
-      // get the accleration
-      // get the velocity going up and down
-
       v = computeTotal(dx, dy);
-      
 
       // Output
       cout.setf(ios::fixed | ios::showpoint);
@@ -269,7 +265,6 @@ int main()
       cout << "\tNew velocity:   (" << dx << ", " << dy << ")m/s\n";
       cout << "\tTotal velocity:  " << v << "m/s\n\n";
    }
-
 
    return 0;
 }
